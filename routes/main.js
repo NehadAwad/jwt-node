ed50cb5e0f24d6b4259b55f335b8700a4df536ef
NehadAwad <nehadakash622@gmail.com>
2022-06-11 22:44:15 +0600

    route middleware added

--- routes/main.js
+++ routes/main.js
@@ -2,3 +2,4 @@ const express = require('express')
 const router = express.Router()
 
 const { login, dashboard } = require('../controllers/main')
+const authMiddleware = require('../middleware/auth')

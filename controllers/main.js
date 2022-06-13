7b3075241c95de1449e6e07bbba0bd489f7275c2
NehadAwad <nehadakash622@gmail.com>
2022-06-13 23:20:40 +0600

    dashboard controller function added

--- controllers/main.js
+++ controllers/main.js
@@ -2,3 +2,9 @@
 const login = async (req, res) => {
     res.send('Fake Login/Reg/SignUp Route')
 }
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({ msg: `Hello, Jhon Doe`, secret: `lucky number ${luckyNumber}`})
+}
+

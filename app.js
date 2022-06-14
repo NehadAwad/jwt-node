616176e76fe47047f770ff66304d2daae162ceb3
NehadAwad <nehadakash622@gmail.com>
2022-06-14 22:12:37 +0600

    test route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const mainRouter = require('./routes/main')
+const testRouter = require('./routes/test')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -13,6 +14,14 @@ app.use(express.static('./public'));
 app.use(express.json());
 
 app.use('/api/v1', mainRouter)
+app.use('/test', testRouter)
+
+app.get("/user", function(req, res){
+  
+  var name = req.query.name
+  console.log("Name :", name)
+ 
+})
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);

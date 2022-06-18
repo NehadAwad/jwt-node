c93bea0ca40616bd4fede9aab91b1e8d095068f1
NehadAwad <nehadakash622@gmail.com>
2022-06-18 14:55:32 +0600

    checked auth

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,6 @@
 
 const jwt = require('jsonwebtoken')
-const { BadRequestError } = require('../errors')
+// const { BadRequestError } = require('../errors')
 
 
 
@@ -13,15 +13,14 @@ app.use(bodyParser.json());
 
 
 const login = async (req, res) => {
-    const { username } = req.query
-    console.log(username)
+    const { username, password } = req.query
     if (!username || !password) {
         throw new BadRequestError('Please provide email and password')
     }
-    const id = new Date().getDate()
-    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-        expiresIn: '30d',
-      });
+    // const id = new Date().getDate()
+    // const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    //     expiresIn: '30d',
+    //   });
     res.send('ok')
 }
 
